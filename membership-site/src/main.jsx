bfb54ecf32c4e2ee442fada15567b178042ddc50
yourbotoxbestie <yourbotoxbestie@gmail.com>
2026-04-04 11:05:13 -0400

    Delete membership-site directory

--- membership-site/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import MembershipPage from './MembershipPage.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <MembershipPage />
-  </React.StrictMode>
-)
