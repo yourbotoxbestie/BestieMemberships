6c481f362fc31a3c0301dfc5e0c1cb5534249b4d
yourbotoxbestie <yourbotoxbestie@gmail.com>
2026-04-04 11:08:53 -0400

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MembershipPage from './MembershipPage.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MembershipPage />
+  </React.StrictMode>
+)
